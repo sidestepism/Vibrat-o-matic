e77b989f904e63347b5f2bb60182c76590e06aa4
sidestepism <fushimi.ryohe.i@gmail.com>
2016-10-06 23:40:33 +0900

    エントリポイントを追加, 起動の仕方などを書いた

---
+++ webapp/index.js
@@ -0,0 +1,21 @@
+var express = require('express')
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+var DEFAULT_SERIAL_COM = "/dev/tty.ABCDEF"
+var serialCOM = process.argv.length > 2 ? process.argv[3] : DEFAULT_SERIAL_COM;
+
+app.use(express.static('static'));
+
+app.get('/', function(req, res){
+  res.sendFile('index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
